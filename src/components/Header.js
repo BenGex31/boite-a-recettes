9da58722f76d2e11bd7c2af4ca2c51dd39801c46
[Benjamin Desole] <benjamin.desole@gmail.com>
2021-03-19 21:30:41 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import { ColorContext } from './Color'
-
 const Header = ({ pseudo }) => {
     const formatPseudo = pseudo => /[aeiouy]/i.test(pseudo[0]) ? `d'${pseudo}`: `de ${pseudo}`
 
     return(
-        <ColorContext.Consumer>
-            {context => (
-                <header style={{ backgroundColor: context.state.color }}>
-                    <h1>La boite à recette {formatPseudo(pseudo)}</h1>
-                </header>
-            )}
-        </ColorContext.Consumer>
+        <header>
+            <h1>La boite à recette {formatPseudo(pseudo)}</h1>
+        </header>
     )
 }
 
